fae13c24b1cf701dabeb87cadfecde956a23dc06
David <D.Huh@t-online.de>
2020-05-14 20:53:41 +0200

    :sparkles: Added class.
    - class: DeliveryStatus

--- lib/backend/Utility.dart
+++ lib/backend/Utility.dart
@@ -1,6 +1,28 @@
+import 'dart:html';
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
-Future<http.Response> fetchDeliveryStatus(id) {
-  return http.get('api-eu.dhl.com/track/shipments?trackingNumber=' + id);
+Future<DeliveryStatus> fetchDeliveryStatus(id) async {
+  final response =
+      await http.get('api-eu.dhl.com/track/shipments?trackingNumber=' + id);
+
+  if (response.statusCode == 200) {
+    return DeliveryStatus.fromJson(json.decode(response.body));
+  } else {
+    throw Exception('Failed to fetch data');
+  }
 }
 
+class DeliveryStatus {
+  int id;
+
+  DeliveryStatus(
+      {this.id});
+
+  factory DeliveryStatus.fromJson(Map<String, dynamic> json) {
+    return DeliveryStatus(
+        id: json['shipments'][0]['id']);
+  }
+}
