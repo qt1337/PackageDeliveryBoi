f8b21539c317a4eb565f6df63fae4f83129b9a92
David <D.Huh@t-online.de>
2020-05-15 12:07:42 +0200

    :sparkles: Added StatusModel.dart
    - also added Models dir

---
+++ lib/models/StatusModel.dart
@@ -0,0 +1,11 @@
+class StatusModel {
+  String id;
+  String status;
+  String name;
+
+  StatusModel(id, status, name) {
+    this.id = id;
+    this.status = status;
+    this.name = name;
+  }
+}
